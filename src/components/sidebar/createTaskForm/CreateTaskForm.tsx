5add622dc949e4b3cf749a62701f1498eb9edf1f
alicefrsr <94556600+alicefrsr@users.noreply.github.com>
2024-01-26 00:41:05 +0100

    TaskSelectFields: interface and enums

--- src/components/sidebar/createTaskForm/CreateTaskForm.tsx
+++ src/components/sidebar/createTaskForm/CreateTaskForm.tsx
@@ -4,6 +4,8 @@ import { TaskTitleField } from './_taskTitleField';
 import { TaskDescriptionField } from './_taskDescriptionField';
 import { TaskDateField } from './_taskDateField';
 import { TaskSelectField } from './_taskSelectField';
+import { Status } from './enums/Status';
+import { Priority } from './enums/Priority';
 
 export const CreateTaskForm: FC = (): ReactElement => {
   return (
@@ -15,7 +17,7 @@ export const CreateTaskForm: FC = (): ReactElement => {
       px={4}
       my={6}
     >
-      <Typography variant="h6" component="h2">
+      <Typography variant="h6" component="h2" my={2}>
         Create a task
       </Typography>
       <Stack sx={{ width: '100%' }} spacing={2}>
@@ -24,9 +26,39 @@ export const CreateTaskForm: FC = (): ReactElement => {
         <TaskDateField />
         <Stack sx={{ width: '100%' }} direction="row" spacing={2}>
           {/* Task status */}
-          <TaskSelectField />
+          <TaskSelectField
+            label="Status"
+            name="status"
+            items={[
+              {
+                value: Status.todo,
+                label: Status.todo.toUpperCase(),
+              },
+              {
+                value: Status.inProgress,
+                label: Status.inProgress.toUpperCase(),
+              },
+            ]}
+          />
           {/* Task Priority */}
-          <TaskSelectField />
+          <TaskSelectField
+            label="Priority"
+            name="priority"
+            items={[
+              {
+                value: Priority.high,
+                label: Priority.high.toUpperCase(),
+              },
+              {
+                value: Priority.normal,
+                label: Priority.normal.toUpperCase(),
+              },
+              {
+                value: Priority.low,
+                label: Priority.low.toUpperCase(),
+              },
+            ]}
+          />
         </Stack>
       </Stack>
     </Box>
--- src/components/sidebar/createTaskForm/_taskSelectField.tsx
+++ src/components/sidebar/createTaskForm/_taskSelectField.tsx
@@ -1,6 +1,6 @@
 import React, { FC, ReactElement } from 'react';
 
-import { ITextField } from './interfaces/ITextField';
+import { ISelectField } from './interfaces/ISelectField';
 
 import {
   FormControl,
@@ -12,21 +12,35 @@ import {
 
 import PropTypes from 'prop-types';
 
-export const TaskSelectField: FC<ITextField> = (props): ReactElement => {
-  //   const {} = props;
+export const TaskSelectField: FC<ISelectField> = (props): ReactElement => {
+  const {
+    value = '',
+    label = 'selectBox',
+    name = 'selectBox',
+    items = [{ value: '', label: 'Add items' }],
+    disabled = false,
+    onChange = (e: SelectChangeEvent) => console.log(e),
+  } = props;
+
   return (
     <FormControl fullWidth size="small">
-      <InputLabel id="status">Status</InputLabel>
+      {/* <InputLabel id="status">Status</InputLabel> */}
+      <InputLabel id={`${name}-id`}>{label}</InputLabel>
       <Select
-        labelId="status"
-        id="status-select"
-        value=""
-        label="status"
-        name="status"
+        labelId={`${name}-id`}
+        // id="status-select"
+        id={`${name}-id-select`}
+        value={value}
+        label={label}
+        name={name}
+        onChange={onChange}
+        disabled={disabled}
       >
-        <MenuItem value={10}>Ten</MenuItem>
-        <MenuItem value={20}>Twenty</MenuItem>
-        <MenuItem value={30}>Thirty</MenuItem>
+        {items.map((item, i) => (
+          <MenuItem key={item.value + i} value={item.value}>
+            {item.label}
+          </MenuItem>
+        ))}
       </Select>
     </FormControl>
   );
@@ -34,5 +48,13 @@ export const TaskSelectField: FC<ITextField> = (props): ReactElement => {
 
 TaskSelectField.propTypes = {
   onChange: PropTypes.func,
+  label: PropTypes.string,
+  name: PropTypes.string,
   disabled: PropTypes.bool,
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+    }).isRequired,
+  ),
 };
